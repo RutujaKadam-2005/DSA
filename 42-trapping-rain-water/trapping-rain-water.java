059d6b4bd99d79ace77fcb4d0c41fb55ade465ba
RutujaKadam-2005 <kadamrutuja328@gmail.com>
2026-08-05 12:02:36 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (82.20%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -4,17 +4,17 @@ class Solution {
        int res = 0, l = 0, r = n - 1; 
        int rMax = height[r], lMax = height[l]; 
         while (l < r) { 
-        if (lMax < rMax) { 
-            l++; 
-            lMax = Math.max(lMax, height[l]); 
-            res += lMax - height[l]; 
-        } else { 
-            r--; 
-            rMax = Math.max(rMax, height[r]); 
-            res += rMax - height[r]; 
+            if (lMax < rMax) { 
+                l++; 
+                lMax = Math.max(lMax, height[l]); 
+                res += lMax - height[l]; 
+            } else { 
+                r--; 
+                rMax = Math.max(rMax, height[r]); 
+                res += rMax - height[r]; 
+            } 
         } 
-        } 
-    return res; 
+        return res; 
     }
         
     
